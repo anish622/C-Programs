a6667f33a3f0e9270e603bf20d339f9cdf830234
anish622 <anishanigowda73@gmail.com>
2025-08-22 10:27:51 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        for (int num = 1; num <= 1000; num++)
+        {
+            int sum = 0;
+            int temp = num;
+            while (temp > 0)
+            {
+                int digit = temp % 10;
+                sum += digit * digit * digit;
+                temp /= 10;
+            }
+            if (sum == num)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
