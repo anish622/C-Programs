011acd7ebb75fe9be6405b02d2a6134fba032200
anish622 <anishanigowda73@gmail.com>
2025-10-03 09:56:20 +0530

    initial commit
    emi calculator

---
+++ EMI.cs/EMI.cs/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace EMI.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a principle loan amount (principle)");
+            double principle = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter a anual rate interest");
+            double anualinterest = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter a  umber of years");
+            double year = Convert.ToDouble(Console.ReadLine());
+            double monthlyinterest = anualinterest / (12 * 100);
+            double v = year * 12;
+            double emi = (principle * monthlyinterest * Math.Pow(1 + monthlyinterest , v)) /
+                 (Math.Pow(1 + monthlyinterest, v) - 1);
+            Console.WriteLine("monthlyemi" + emi);
+
+
+        }
+    }
+}
