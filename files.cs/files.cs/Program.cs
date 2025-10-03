a03c96fd71d4f438d6d19243ca7903ee0a910896
anish622 <anishanigowda73@gmail.com>
2025-10-03 10:18:08 +0530

    initial commit

---
+++ files.cs/files.cs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+class DirectoryFileLister
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the full path of the directory:");
+        string directoryPath = Console.ReadLine();
+
+        if (Directory.Exists(directoryPath))
+        {
+            Console.WriteLine($"\nFiles in '{directoryPath}':\n");
+
+            string[] files = Directory.GetFiles(directoryPath);
+
+            foreach (string file in files)
+            {
+                Console.WriteLine(Path.GetFileName(file));
+            }
+        }
+        else
+        {
+            Console.WriteLine("The specified directory does not exist.");
+        }
+    }
+}
+
