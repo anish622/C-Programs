a4897b2407c764e62911a92727f6b84bc339443e
anish622 <anishanigowda73@gmail.com>
2025-10-10 10:31:27 +0530

    initial commit

---
+++ queue.cs/queue.cs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+class Program
+{
+    public static void Show()
+    {
+        Queue<string> queue = new Queue<string>();
+        queue.Enqueue("Anish");
+        queue.Enqueue("Charlie");
+        queue.Enqueue("raam");
+        queue.Enqueue("Bob");
+        queue.Dequeue();
+
+        foreach (string name in queue)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
+
+class MyClass
+{
+    static void Main()
+    {
+        Program.Show();
+    }
+
+}
