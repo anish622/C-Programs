4233c0e96ad8a110e615e97828e18eac0ef11995
anish622 <anishanigowda73@gmail.com>
2025-10-10 10:44:24 +0530

    initial commit

---
+++ DataEncapsulation/DataEncapsulation/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace DataEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Person Person1 = new Person();
+
+            Person1.Name = "dhaiwik";
+            Person1.Age = 25;
+            Console.WriteLine($"Name: {Person1.Name} Age: {Person1.Age}");
+        }
+    }
+}
---
+++ delegate.cs/delegate.cs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+delegate void Greet();
+class program
+{
+    static void SayHello()
+    {
+        Console.WriteLine("Hello good morning students!");
+    }
+
+    static void Main()
+    {
+        Greet greetdelegate = SayHello;
+        greetdelegate();
+    }
+
+}
